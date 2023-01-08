38cb975468588aa2067cf75a9bd9ecf4fe867e52
AhmedEid3 <ahmed.eid3@outlook.com>
2023-01-08 09:01:27 +0200

    Style navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/index.html"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+      },
+      colors: {
+        primary: "#141943",
+        secondary: "#2F80ED",
+      },
+      borderRadius: {
+        "8xl": "4rem",
+      },
+    },
   },
   plugins: [],
 };
