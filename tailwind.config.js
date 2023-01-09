8483868b67e75a9f9fca99627ee78a75661a21e1
AhmedEid3 <ahmed.eid3@outlook.com>
2023-01-09 22:21:41 +0200

    Add style to Overview Section - Add style to Sponsors Section - Add style to Trending NFTs Section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,26 @@ module.exports = {
   content: ["./public/index.html"],
   theme: {
     extend: {
+      screens: {
+        "2xl": "1280px",
+      },
+
       container: {
         center: true,
       },
       colors: {
         primary: "#141943",
         secondary: "#2F80ED",
+        accent: "#6A58F1",
+        "green-light": "#70FF00",
       },
       borderRadius: {
         "8xl": "4rem",
+      },
+
+      fontFamily: {
+        clash: "Clash Display",
+        Poppins: "Poppins",
       },
     },
   },
